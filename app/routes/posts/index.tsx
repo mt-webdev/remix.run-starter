a98431810a3ebc1eac359201fdf3ce255c55e6b2
Traspel, Marco <m.traspel@controlexpert.com>
2021-12-10 16:53:26 +0100

    post list styling

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,8 +1,16 @@
 // remix.run blogs index.tsx
 
-import { Link, LoaderFunction, useLoaderData } from "remix";
+import { Link, LinksFunction, LoaderFunction, useLoaderData } from "remix";
+import postListStylesUrl from "~/styles/post-list.css";
 import { PostModel } from "../../models/post.models";
 import { postsDataService } from "../../services/posts-data.service";
+
+export const links: LinksFunction = () => [
+  {
+    rel: "stylesheet",
+    href: postListStylesUrl,
+  },
+];
 
 type LoaderData = Omit<PostModel, "content">[];
 export const loader: LoaderFunction = async () => {
@@ -13,7 +21,7 @@ export default function Blogs() {
   const posts = useLoaderData<LoaderData>();
 
   return (
-    <div>
+    <div className="page__post-list">
       <h1>Posts</h1>
       <ul>
         {posts.map((post) => (
--- data/posts/wef.md
+++
@@ -1,4 +0,0 @@
----
-title: wef
----
-wef
