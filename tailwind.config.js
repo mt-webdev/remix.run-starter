1eb5be7fa9e53c68e7bf3fe934a2232972ed8362
Traspel, Marco <m.traspel@controlexpert.com>
2021-12-15 10:01:12 +0100

    theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,47 @@
+function withOpacity(variableName) {
+  return ({opacityValue}) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}), ${opacityValue})`
+    }
+    return `rgb(var(${variableName}))`
+  }
+}
+
 module.exports = {
   content: ['./app/**/*.{html,tsx}'],
   theme: {
-    //  colors: {
-    //   'blue': '#1fb6ff',
-    //   'purple': '#7e5bef',
-    //   'pink': '#ff49db',
-    //   'orange': '#ff7849',
-    //   'green': '#13ce66',
-    //   'yellow': '#ffc82c',
-    //   'gray-dark': '#273444',
-    //   'gray': '#8492a6',
-    //   'gray-light': '#d3dce6',
-    // },
-    extend: {},
+    extend: {
+      textColor: {
+        skin: {
+          base: withOpacity('--theme-text-base'),
+          highlight: withOpacity('--theme-text-highlight'),
+          muted: withOpacity('--theme-text-muted'),
+          button: withOpacity('--theme-button-text'),
+          'button-muted': withOpacity('--theme-button-text-muted'),
+          'button-inverted': withOpacity('--theme-button-inverted-text'),
+          'button-inverted-muted': withOpacity('--theme-button-inverted-muted'),
+        },
+      },
+      backgroundColor: {
+        skin: {
+          base: withOpacity('--theme-background-base'),
+          highlight: withOpacity('--theme-background-highlight'),
+          'button-bg': withOpacity('--theme-button-background'),
+          'button-inverted-bg': withOpacity(
+            '--theme-button-inverted-background',
+          ),
+        },
+      },
+      gridTemplateRows: {
+        'app-sm': 'auto 1fr auto',
+        'app-md': 'auto 1fr auto',
+      },
+      gradientColorStops: {
+        skin: {
+          hue: withOpacity('--theme-background-highlight'),
+        },
+      },
+    },
   },
   plugins: [],
 }
